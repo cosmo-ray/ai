ce4e14135930e86b2b0727488ebd96d5412b9a82
matthias <uso.cosmo.ray@gmail.com>
2019-04-12 21:49:09 +0200

    map

--- start.c
+++ start.c
@@ -22,6 +22,8 @@
 #include <yirl/entity.h>
 #include <yirl/game.h>
 
+#define NB_MONSTERS 2
+
 static void print_mob(Entity *mob)
 {
 	YE_FOREACH(mob, str) {
@@ -29,9 +31,51 @@ static void print_mob(Entity *mob)
 	}
 }
 
+static void draw_level(Entity *ai, Entity *level)
+{
+	Entity *txt = yeGet(ai, "text");
+	Entity *pjp = yeGet(ai, "pjp");
+	Entity *pj = yeGet(ai, "pj");
+	Entity *monsters = yeGet(ai, "monsters");
+
+	for (int i = 0; i < yeLen(level); ++i) {
+		Entity *ll = yeGet(level, i);
+		yePushAt(txt, ll, i + 2);
+
+	}
+
+	int px = ywPosX(pjp);
+	int py = ywPosY(pjp);
+	int pjl = yeLen(pj);
+	for (int i = 0; i < pjl; ++i) {
+		yeStringReplaceStrAt(yeGet(txt, py - pjl + i + 3),
+				     yeGetStringAt(pj, i),
+				     px);
+	}
+	for (int j = 0; j < NB_MONSTERS; ++j) {
+		int mt = yeGetIntAt(yeGet(yeGet(ai, "msp"), j), 0);
+ 		Entity *mpos = yeGet(yeGet(yeGet(ai, "msp"), j), 1);
+		Entity *mon = yeGet(monsters, mt);
+
+		px = ywPosX(mpos);
+		py = ywPosY(mpos);
+		if (px > 0) {
+			int mjl = yeLen(mon);
+			for (int k = 0; k < mjl; ++k) {
+				yeStringReplaceStrAt(yeGet(txt, py - mjl + k + 3),
+						     yeGetStringAt(mon, k),
+						     px);
+			}
+		}
+	}
+}
+
 void *ai_action(int nbArgs, void **args)
 {
+	Entity *ai = args[0];
+
 	printf("action !\n");
+	draw_level(ai, yeGet(ai, "level"));
 	return (void *)ACTION;
 }
 
@@ -40,7 +84,6 @@ static Entity *ai_levels(void)
 	Entity *r = yeCreateArray(NULL, NULL);
 
 #include "levels.c"
-
 	print_mob(yeGet(r, 0));
 	return r;
 }
@@ -51,7 +94,8 @@ void *ai_init(int nbArgs, void **args)
 	yeAutoFree Entity *levels = ai_levels();
 	Entity *level = yeGet(levels, 0);
 	Entity *pj;
-	Entity *txt;
+	Entity *monsters;
+	Entity *msp;
 
 	printf("ai, yume no ai !\n");
 	YEntityBlock {
@@ -80,33 +124,40 @@ void *ai_init(int nbArgs, void **args)
 		ai.background = "rgba: 255 255 255 255";
 		ai.fmt = "yirl";
 		ai.action = ai_action;
+		// monsters pos
+		ai.msp = [];
 		ai["text-align"] = "center";
 		ai["turn-length"] = 200000;
 	}
 
-	//printf("%p - %p\n", yeGet(ai, "lv"), yeGet(ai, "lvs"));
-	txt = yeGet(ai, "text");
 	pj = yeGet(ai, "pj");
 	print_mob(yeGetByStr(ai, "monsters.0"));
 	print_mob(yeGetByStr(ai, "monsters.1"));
 	print_mob(yeGet(levels, 0));
 	print_mob(pj);
+	monsters = yeGet(ai, "monsters");
+	msp = yeGet(ai, "msp");
+	/* The Great Replacement */
 	for (int i = 0; i < yeLen(level); ++i) {
 		Entity *ll = yeGet(level, i);
-		int px = yeStrChrIdx(ll, 'P');
+		int px = yeStrChrIdx(ll, '*');
 		printf("chr: %d\n", px);
 		if (px > 0) {
-			int pjl = yeLen(pj);
-			for (int j = 0; j < pjl; ++j) {
-				yeStringReplaceStrAt(yeGet(level, i - pjl + j + 1),
-						     yeGetStringAt(pj, j),
-						     px);
-			}
 			YEntityBlock { ai.pjp = [px, i]; };
 		}
-		yePushAt(txt, ll, i + 2);
+		for (int j = 0; j < NB_MONSTERS; ++j) {
+			Entity *mon = yeGet(monsters, j);
+			px = yeStrChrIdx(ll, '0' + j);
+			if (px > 0) {
+				Entity *mp = yeCreateArray(msp, NULL);
+				YEntityBlock { mp = [j, [px, i]]; };
+			}
+		}
 	}
+	draw_level(ai, level);
 	ywPosPrint(yeGet(ai, "pjp"));
+	ywPosPrint(yeGet(yeGet(yeGet(ai, "msp"), 0), 1));
+	ywPosPrint(yeGet(yeGet(yeGet(ai, "msp"), 1), 1));
 	printf("%p\n", yeGet(yeGet(ai, "text"), 0));
 	void *ret = ywidNewWidget(ai, "text-screen");
 	return ret;
